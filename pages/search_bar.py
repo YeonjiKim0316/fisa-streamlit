acf7bf950981379d08b6d0ca15e23ff45fc95a1c
YeonjiKim0316 <kimyeonji3@gmail.com>
2025-01-14 16:43:04 +0900

    streamlit test

---
+++ pages/search_bar.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+
+# text를 입력하는 검색창을 하나 만듭니다.
+# ani_list에 있는 글자가 일부라도 들어가면
+# img_list에 있는 해당 그림이 출력되는 검색창을 하나 만들어주세요
+ani_list = ['짱구는못말려', '몬스터','릭앤모티']
+img_list = ['https://i.imgur.com/t2ewhfH.png', 
+            'https://i.imgur.com/ECROFMC.png', 
+            'https://i.imgur.com/MDKQoDc.jpg']
