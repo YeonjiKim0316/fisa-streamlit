7c14469b97670da35d45cde828416f6deb700a61
YeonjiKim0316 <kimyeonji3@gmail.com>
2025-01-14 17:04:57 +0900

    streamlit test

--- pages/search_bar.py
+++ pages/search_bar.py
@@ -8,3 +8,13 @@ ani_list = ['짱구는못말려', '몬스터','릭앤모티']
 img_list = ['https://i.imgur.com/t2ewhfH.png', 
             'https://i.imgur.com/ECROFMC.png', 
             'https://i.imgur.com/MDKQoDc.jpg']
+
+search = st.text_input("애니메이션을 입력하세요")
+print(search)
+
+for ani in ani_list:
+    if search in ani:
+        img_idx = ani_list.index(ani)
+
+if search != '': # 초기상태를 이미지없이 실행
+    st.image(img_list[img_idx])
